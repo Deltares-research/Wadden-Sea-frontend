41a85885a7fde3b97970008d245f24d04bbd60b0
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-10-10 12:24:47 +0200

    fixed path to assets

--- src/stores/app.js
+++ src/stores/app.js
@@ -9,7 +9,7 @@ export const useAppStore = defineStore('app', {
   actions: {
     async loadItems() {
       if (this.itemsLoaded) return
-      const res = await fetch(import.meta.env.BASE_URL + 'public/assets/items.json')
+      const res = await fetch(import.meta.env.BASE_URL + 'assets/items.json')
       this.items = await res.json()
       this.itemsLoaded = true
     } },
